93349e375bcce91f37b8539a31eeb6f319109d4e
Yuxuan Lin <exceptionlin@outlook.com>
2016-09-01 14:12:51 +1000

    Initial Commit

---
+++ LocationReminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationReminder
+//
+//  Created by linyuxuan on 1/09/2016.
+//  Copyright © 2016 yuxuanlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
